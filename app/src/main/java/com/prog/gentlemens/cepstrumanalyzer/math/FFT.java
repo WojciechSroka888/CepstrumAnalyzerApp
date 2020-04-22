88255afcaeea0fd3ecd4c1c33eea296fa44e3a0a
Wojciech Sroka <ws.sroka@gmail.com>
2020-04-22 20:48:28 +0200

    add math package

--- app/src/main/java/com/prog/gentlemens/cepstrumanalyzer/math/FFT.java
+++ app/src/main/java/com/prog/gentlemens/cepstrumanalyzer/math/FFT.java
@@ -1,92 +1,83 @@
+package com.prog.gentlemens.cepstrumanalyzer.math;
 
-
-package com.prog.gentlemens.cepstrumanalyzer;
-
-public class fft 
-{
-
+public final class FFT {
+	
+	private FFT(){
+	}
+	
+	
 	//Replaces data[1..2*nn] by its discrete Fourier transform, if isign is input as 1;
 	//or replaces data[1..2*nn] by nn times its inverse discrete Fourier transform,
 	//if isign is input as -1. data is a complex array of length nn or, equivalently,
 	//a real array of length 2*nn. nn MUST be an integer power of 2 (this is not checked for!).
-	public static int suma(int a, int b)
-	{
+	public static int suma(int a, int b) {
 		return a + b;
 	}
 	
-	public static void Four1(double []data, int isign)
-	{
-		int nn = data.length/2;
-		int n,mmax,m,j,istep,i;
+	public static void Four1(double[] data, int isign) {
+		int nn = data.length / 2;
+		int n, mmax, m, j, istep, i;
 		double wtemp, wr, wpr, wpi, wi, theta;
 		double tempr, tempi;
-
+		
 		n = nn << 1;
 		j = 1;
-		for (i = 1; i < n; i += 2) 
-		{
-			if (j > i) 
-			{
-				tempr = data[j-1]; 
-				data[j-1]=data[i-1]; 									
-				data[i-1]=tempr;
-				tempr=data[j]; 
-				data[j]=data[i]; 									
-				data[i]=tempr;
+		for (i = 1; i < n; i += 2) {
+			if (j > i) {
+				tempr = data[j - 1];
+				data[j - 1] = data[i - 1];
+				data[i - 1] = tempr;
+				tempr = data[j];
+				data[j] = data[i];
+				data[i] = tempr;
 			}
-
-		m = nn;
-
-			while (m >= 2 && j > m) 
-			{
+			
+			m = nn;
+			
+			while (m >= 2 && j > m) {
 				j -= m;
 				m >>= 1;
 			}
 			j += m;
 		}
-
+		
 		mmax = 2;
-
-		while (n > mmax) 
-		{
+		
+		while (n > mmax) {
 			istep = mmax << 1;
-			theta = isign * (6.28318530717959/mmax);
-			wtemp = Math.sin(0.5*theta);
-			wpr = -2.0*wtemp*wtemp;
+			theta = isign * (6.28318530717959 / mmax);
+			wtemp = Math.sin(0.5 * theta);
+			wpr = -2.0 * wtemp * wtemp;
 			wpi = Math.sin(theta);
 			wr = 1.0;
 			wi = 0.0;
-
-			for (m = 1; m < mmax; m += 2) 
-			{
-				for (i = m; i <= n; i += istep) 
-				{
+			
+			for (m = 1; m < mmax; m += 2) {
+				for (i = m; i <= n; i += istep) {
 					j = i + mmax;
-					tempr = (wr*data[j-1]-wi*data[j]);
-					tempi = (wr*data[j]+wi*data[j-1]);
-					data[j-1] = data[i-1]-tempr;
-					data[j] = data[i]-tempi;
-					data[i-1] += tempr;
+					tempr = (wr * data[j - 1] - wi * data[j]);
+					tempi = (wr * data[j] + wi * data[j - 1]);
+					data[j - 1] = data[i - 1] - tempr;
+					data[j] = data[i] - tempi;
+					data[i - 1] += tempr;
 					data[i] += tempi;
 				}
-
-				wr=(wtemp=wr)*wpr-wi*wpi+wr;
-				wi=wi*wpr+wtemp*wpi+wi;
+				
+				wr = (wtemp = wr) * wpr - wi * wpi + wr;
+				wi = wi * wpr + wtemp * wpi + wi;
 			}
-
-			mmax=istep;
-
+			
+			mmax = istep;
+			
 		}
-
-		if (isign == -1)
-		{
-			for (i = 0; i < n ; i++)
-			{
-				data[i]/=nn;
+		
+		if (isign == -1) {
+			for (i = 0; i < n; i++) {
+				data[i] /= nn;
 			}
 		}
 	}
-
+	
 	//Calculates the Fourier transform of a set of n real-valued data points.
 	//Replaces this data (which is stored in array data[1..n]) by the positive
 	//frequency half of its complex Fourier transform. The real-valued first
@@ -95,92 +86,77 @@ public class fft
 	//also calculates the inverse transform of a complex data array
 	//if it is the transform of real data.
 	//(Result in this case must be multiplied by 2/n.)
- 
-	public static void RealFT(double []data, int isign)
-	{
+	
+	public static void RealFT(double[] data, int isign) {
 		int n = data.length;
-		int i,i1,i2,i3,i4;	
-		double c1 = 0.5f,c2,h1r,h1i,h2r,h2i;
+		int i, i1, i2, i3, i4;
+		double c1 = 0.5f, c2, h1r, h1i, h2r, h2i;
 		double wr, wi, wpr, wpi, wtemp, theta;
-		theta = Math.PI / ( n >> 1 );
-
-		if (isign == 1) 
-		{
-			c2 =- 0.5f;
-			Four1( data, 1 );
-		} 
-
-		else 
-		{
+		theta = Math.PI / (n >> 1);
+		
+		if (isign == 1) {
+			c2 = -0.5f;
+			Four1(data, 1);
+		} else {
 			c2 = 0.5f;
 			theta = -theta;
 		}
-
+		
 		wtemp = Math.sin(0.5 * theta);
-		wpr =- 2.0 * wtemp * wtemp;
+		wpr = -2.0 * wtemp * wtemp;
 		wpi = Math.sin(theta);
-		wr = 1.0+wpr;
+		wr = 1.0 + wpr;
 		wi = wpi;
-
-		for (i = 2; i <= ( n>>2 ); i++) 
-		{
-			i4=1+(i3=n-(i1=(i2=i+i-1)-1));
-			h1r=c1*(data[i1]+data[i3]);
-			h1i=c1*(data[i2]-data[i4]);
-			h2r = -c2*(data[i2]+data[i4]);
-			h2i=c2*(data[i1]-data[i3]);
-			data[i1]=(h1r+wr*h2r-wi*h2i);
-			data[i2]=(h1i+wr*h2i+wi*h2r);
-			data[i3]=(h1r-wr*h2r+wi*h2i);
-			data[i4]=(-h1i+wr*h2i+wi*h2r);
-			wr=(wtemp=wr)*wpr-wi*wpi+wr;
-			wi=wi*wpr+wtemp*wpi+wi;
+		
+		for (i = 2; i <= (n >> 2); i++) {
+			i4 = 1 + (i3 = n - (i1 = (i2 = i + i - 1) - 1));
+			h1r = c1 * (data[i1] + data[i3]);
+			h1i = c1 * (data[i2] - data[i4]);
+			h2r = -c2 * (data[i2] + data[i4]);
+			h2i = c2 * (data[i1] - data[i3]);
+			data[i1] = (h1r + wr * h2r - wi * h2i);
+			data[i2] = (h1i + wr * h2i + wi * h2r);
+			data[i3] = (h1r - wr * h2r + wi * h2i);
+			data[i4] = (-h1i + wr * h2i + wi * h2r);
+			wr = (wtemp = wr) * wpr - wi * wpi + wr;
+			wi = wi * wpr + wtemp * wpi + wi;
 		}
-
-		if (isign == 1) 
-		{
-			data[0] = (h1r=data[0])+data[1];
-			data[1] = h1r-data[1];
-		} 
-
-		else 
-		{
-			data[0]=c1*((h1r=data[0])+data[1]);
-			data[1]=c1*(h1r-data[1]);
-			Four1(data,-1);
+		
+		if (isign == 1) {
+			data[0] = (h1r = data[0]) + data[1];
+			data[1] = h1r - data[1];
+		} else {
+			data[0] = c1 * ((h1r = data[0]) + data[1]);
+			data[1] = c1 * (h1r - data[1]);
+			Four1(data, -1);
 		}
 	}
-
-	private static double OknoWelcha2(int i, int dl) 
-	{
-		double pom=(i-0.5f*dl)/(0.5f*dl);
-		return (1.0-pom*pom*pom*pom);
+	
+	private static double OknoWelcha2(int i, int dl) {
+		double pom = (i - 0.5f * dl) / (0.5f * dl);
+		return (1.0 - pom * pom * pom * pom);
 	}
 	
 	
 	@SuppressWarnings("unused")
-	private static double OknoProstokatne(int i, int dl)
-	{
+	private static double OknoProstokatne(int i, int dl) {
 		return 1.0;
 	}
-	public static double[] Widmo(short []dane, int dlOkna, int n)
-	{
+	
+	public static double[] Widmo(short[] dane, int dlOkna, int n) {
 		double widmo[] = new double[dlOkna / 2 + 1];
 		double okno[] = new double[dlOkna];
 		double daneFFT[] = new double[dlOkna];
 		double sumWspOkna = 0.0;
-
-		for (int i = 0; i < dlOkna; i++) 
-		{
+		
+		for (int i = 0; i < dlOkna; i++) {
 			okno[i] = OknoWelcha2(i, dlOkna);
 			//okno[i] = OknoProstokatne(i, dlOkna);
 			sumWspOkna += okno[i] * okno[i];
 		}
-
-		for (int j = 0; j < n; j++) 
-		{
-			for (int i = 0; i < dlOkna; i++)
-			{				
+		
+		for (int j = 0; j < n; j++) {
+			for (int i = 0; i < dlOkna; i++) {
 				daneFFT[i] = dane[j * dlOkna + i] * okno[i];
 			}
 			
@@ -189,20 +165,20 @@ public class fft
 			widmo[0] += daneFFT[0] * daneFFT[0];
 			widmo[dlOkna / 2] += daneFFT[1] * daneFFT[1]; // fNyquist
 			
-			for (int i = 1, k = 2; i < dlOkna / 2; i++, k += 2) 
-			{
-				widmo[i] += 2 * (daneFFT[k] * daneFFT[k] + daneFFT[k+1] * daneFFT[k+1]) ;
+			for (int i = 1, k = 2; i < dlOkna / 2; i++, k += 2) {
+				widmo[i] += 2 * (daneFFT[k] * daneFFT[k] + daneFFT[k + 1] * daneFFT[k + 1]);
 			}
 		}
-
-		for (int i = 0; i < widmo.length; i++)
-		{
+		
+		for (int i = 0; i < widmo.length; i++) {
 			widmo[i] /= (sumWspOkna * n * dlOkna);
 		}
 		
-		for (int i = 0; i < widmo.length; i++)
-			widmo[i] = Math.sqrt(2*widmo[i]);
-
+		for (int i = 0; i < widmo.length; i++) {
+			widmo[i] = Math.sqrt(2 * widmo[i]);
+		}
+		
 		return widmo;
 	}
+	
 }
---
+++ app/src/main/java/com/prog/gentlemens/cepstrumanalyzer/math/MathOperations.java
@@ -0,0 +1,325 @@
+package com.prog.gentlemens.cepstrumanalyzer.math;
+
+import com.prog.gentlemens.cepstrumanalyzer.enums.NameOfMathFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.log10;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+/**
+ * In this class most of the data type is primitive - because in most cases the mathematical operations on primitives
+ * are faster then on objects
+ */
+public final class MathOperations {
+	
+	private MathOperations() {
+	}
+	
+	//TODO tests: calculateFFT()
+	public static double calculateFFT(short[] inputData, int sampleRate, double maxFrequency, double minFrequency) {
+		validateCalculateFFT(inputData, sampleRate, maxFrequency, minFrequency);
+		
+		double[] tempData = new double[inputData.length];
+		double[] blackmannWindow = MathOperations.calculateBlackmannWindow(inputData.length);     // normally inputData.length should = 1024
+		
+		// FFT
+		for (int i = 0; i < inputData.length; ++i) {
+			tempData[i] = inputData[i] * blackmannWindow[i];
+		}
+		
+		FFT.RealFT(tempData, 1);
+		
+		// LN and ABS
+		for (int i = 0; i < tempData.length; i = i + 2) {
+			if (tempData[i] == 0) {
+				tempData[i + 1] = 0;
+				continue;
+			}
+			
+			tempData[i] = Math.sqrt(tempData[i] * tempData[i] + tempData[i + 1] * tempData[i + 1]);
+			tempData[i + 1] = 0;
+			
+			tempData[i] = Math.log(tempData[i]);
+		}
+		
+		// IFFT
+		FFT.RealFT(tempData, -1);
+		tempData[0] = tempData[1] = tempData[tempData.length - 1] = tempData[tempData.length - 2] = 0;
+		
+		// MAX
+		double[] max = MathOperations.findMax(tempData, sampleRate, maxFrequency, minFrequency);
+		//SEND RESULT
+		
+		//System.out.println("fft: " + max[0]);
+		//[0] max | [1] shim
+		return max[0];
+	}
+	
+	private static void validateCalculateFFT(short[] inputDataToValidate, int sampleRateToValidate, double maxFrequencyToValidate, double minFrequencyToValidate) {
+		String message = null;
+		if (inputDataToValidate == null) {
+			message += "short array can not be null ";
+		}
+		if (sampleRateToValidate <= 0) {
+			message += " sampleRate has to be greater then zero ";
+		}
+		if (maxFrequencyToValidate <= 0) {
+			message += " maxFrequency has to be greater then zero ";
+		}
+		if (minFrequencyToValidate <= 0) {
+			message += " minFrequency has to be greater then zero ";
+		}
+		if (maxFrequencyToValidate < minFrequencyToValidate) {
+			message += " maxFrequency has to be greater or equal the minFrequency ";
+		}
+		if (message != null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	//TODO tests: findMax()
+	public static double[] findMax(double[] inputData, int fs, double maxFrequency, double minFrequency) {
+		validateFindMax(inputData, fs, maxFrequency, minFrequency);
+		
+		double maxFreq = 0;
+		double ampForMaxFreq = 0;
+		int startIndex = round(fs / maxFrequency);        //appropriate starting index
+		int endIndex = round(fs / minFrequency);
+		int index = 0;                                 //number of maximum array
+		//when only 0 instead of index = start
+		double tempMaxFreq = inputData[startIndex];     //looking for maximum
+		
+		for (int i = startIndex; i < (endIndex > inputData.length - 1 ? inputData.length - 1 : endIndex); ++i) {
+			if (tempMaxFreq < inputData[i + 1]) {
+				tempMaxFreq = inputData[i + 1];
+				index = i + 1;
+			}
+		}
+		
+		if (index != 0) {
+			maxFreq = ((double) fs) / index;        // 1 / ( index * 1 / ( fs ) )
+			ampForMaxFreq = inputData[index];    //ampForMaxFreq[i] = tempMaxFreq;
+		} else {
+			if (index == 0) {
+				maxFreq = 0;
+				ampForMaxFreq = 0;
+			}
+		}
+		
+		//System.out.println( index + '-' + maxFreq[i] + '-' + tempMaxFreq);
+		return new double[]{maxFreq, ampForMaxFreq};
+	}
+	
+	private static void validateFindMax(double[] inputDataToValidate, int sampleRateToValidate, double maxFrequencyToValidate, double minFrequencyToValidate) {
+		String message = null;
+		if (inputDataToValidate == null) {
+			message += "double array can not be null ";
+		}
+		if (sampleRateToValidate <= 0) {
+			message += " sampleRate has to be greater then zero ";
+		}
+		if (maxFrequencyToValidate <= 0) {
+			message += " maxFrequency has to be greater then zero ";
+		}
+		if (minFrequencyToValidate <= 0) {
+			message += " minFrequency has to be greater then zero ";
+		}
+		if (maxFrequencyToValidate < minFrequencyToValidate) {
+			message += " maxFrequency has to be greater or equal the minFrequency ";
+		}
+		if (message != null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	public static double calculateMathFunctionInSteps(NameOfMathFunction nameOfMathFunction, short[] recordA, short[] recordB, int step, int fs, double maxFrequency, double minFrequency) {
+		validateCalculateMathFunctionInSteps(nameOfMathFunction, recordA, recordB, step);
+            /*
+                This function copies values from recordA to recordB - d so as result it generates set of arrays
+                that includes all recordA array and set of arrays with elements from recordA and recordB array
+                -> it does not create array that includes only values from recordB array !
+                -> it is created in the next iteration of averageOnFly() function
+                 for recordA.length == recordB.length
+             */
+		
+		double result = 0;
+		double tempCalculate = 0;
+		
+		int numberOfParts = round((recordB.length) / (float) step);
+		int counter = 0;
+		
+		short[] tempRecordArray = new short[recordB.length];
+		
+		for (int i = 0; i < numberOfParts; ++i) {
+			try {
+				// from first frame (array)
+				System.arraycopy(recordA, i * step, tempRecordArray, 0, tempRecordArray.length - i * step);
+				
+				// from second frame (array)
+				System.arraycopy(recordB, 0, tempRecordArray, tempRecordArray.length - i * step, i * step);
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println(" exception in System.arraycopy() in averageOnFly()");
+				continue;
+			}
+			
+			if (nameOfMathFunction == NameOfMathFunction.CALCULATE_FFT) {
+				tempCalculate = calculateFFT(tempRecordArray, fs, maxFrequency, minFrequency);
+			} else {
+				if (nameOfMathFunction == NameOfMathFunction.CALCULATE_RMS) {
+					tempCalculate = calculateRMS(tempRecordArray);
+				}
+			}
+			
+			// do not add values from tempRecordArray that max is = 0 -> it is silence
+			if (tempCalculate != 0) {
+				result = tempCalculate + result;
+				counter = counter + 1;
+			}
+		}
+		
+		return result / counter;
+	}
+	
+	private static void validateCalculateMathFunctionInSteps(NameOfMathFunction nameOfMathFunction, short[] recordAToValidate, short[] recordBToValidate, int stepToValidate) {
+		String message = null;
+		if (nameOfMathFunction == null) {
+			message += " nameOfMathFunction can not be null ";
+		}
+		if (recordAToValidate == null) {
+			message += " short array recordA can not be null ";
+		}
+		if (recordBToValidate == null) {
+			message += " short array recordB can not be null ";
+		}
+		if (recordAToValidate != null && recordBToValidate != null) {
+			if (recordAToValidate.length != recordBToValidate.length) {
+				message += " length of short array recordA and short array recordB has to be equal ";
+			}
+			if (recordAToValidate.length <= 0 || recordBToValidate.length <= 0) {
+				message += " length of short array recordA and short array recordB has to be greater then 0 ";
+			}
+		}
+		if (stepToValidate <= 0) {
+			message += " step has to be greater then zero ";
+		}
+		if (message != null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+	/**
+	 * @param freqArray double [] - array that contains frequencies
+	 * @return double - calculated jitter value
+	 */
+	//TODO tests: calculateJitter()
+	public static double calculateJitter(double[] freqArray) {
+		validateCalculateJitter(freqArray);
+		
+		// make new array with periods based on freqArray [] input
+		List<Double> periodArray = new ArrayList<>();
+		double jitter = 0;
+		
+		// validate data - without 0 and NaN values
+		for (double singleDoubleElement : freqArray) {
+			if (singleDoubleElement != 0 && !Double.isNaN(singleDoubleElement)) {
+				periodArray.add(Math.pow(singleDoubleElement, -1));
+			}
+		}
+		
+		for (int i = 1; i < periodArray.size(); ++i) {
+			jitter = jitter + Math.abs(periodArray.get(i) - periodArray.get(i - 1));
+		}
+		
+		//jitter = jitter * 1 / (N - 1)
+		jitter = jitter * Math.pow(periodArray.size() - 1, -1);
+		
+		// the previous value is in [s], change to [ms]
+		jitter = jitter * 1000;
+		
+		return jitter;
+	}
+	
+	private static void validateCalculateJitter(double[] freqArrayToValidate) {
+		if (freqArrayToValidate == null) {
+			throw new IllegalArgumentException(" freqArray can not be null ");
+		}
+	}
+	
+	/**
+	 * @param ampArray double [] - array that contains amplitudes
+	 * @return double - calculated shimmer value
+	 */
+	//TODO tests: calculateShimmer()
+	public static double calculateShimmer(double[] ampArray) {
+		validateCalculateShimmer(ampArray);
+		
+		double shimmer = 0;
+		double counter = 0;
+		
+		for (int i = 0; i < ampArray.length - 1; ++i) {
+			if (ampArray[i] != 0 && !Double.isNaN(ampArray[i])) {
+				if (ampArray[i + 1] != 0 && !Double.isNaN(ampArray[i + 1])) {
+					shimmer = shimmer + Math.abs(20 * Math.log10(ampArray[i + 1] / ampArray[i]));
+					counter = counter + 1;
+				}
+			}
+		}
+		
+		shimmer = shimmer * Math.pow(counter - 1, -1);
+		return shimmer;
+	}
+	
+	private static void validateCalculateShimmer(double[] freqArrayToValidate) {
+		if (freqArrayToValidate == null) {
+			throw new IllegalArgumentException(" freqArray can not be null ");
+		}
+	}
+	
+	public static double[] calculateBlackmannWindow(int size) {
+		double[] windowOut = new double[size];
+		
+		for (int i = 0; i < size; ++i) {
+			windowOut[i] = 0.42 - 0.5 * Math.cos(2 * Math.PI * i / (size - 1)) + 0.08 * Math.cos(4 * Math.PI * i / (size - 1));
+		}
+		
+		return windowOut;
+	}
+	
+	//TODO tests: dbMeter()
+	public static double calculateDB(double inputData) {
+		return 10 * log10(inputData);
+	}
+	
+	//TODO tests: rmsMeter()
+	public static double calculateRMS(short[] shortInputData) {
+		validateCalculateRMS(shortInputData);
+		
+		double result = 0;
+		
+		for (short singleShortElement : shortInputData) {
+			// + pow(2, 8) -> adds 2^8 to move -2^8 value to 0 -> move minimum value to 0 not to -2^8
+			result = result + pow((singleShortElement + pow(2, 0)), 2);
+		}
+		
+		return sqrt(result / shortInputData.length);
+	}
+	
+	private static void validateCalculateRMS(short[] shortInputDataToValidate) {
+		if (shortInputDataToValidate == null) {
+			throw new IllegalArgumentException(" short array can not be null ");
+		}
+	}
+	
+	public static int round(double value) {
+		if ((value - (int) value) >= 0.5)            //(int) value == floor(value)
+		{
+			return (int) Math.floor(value + 0.5);
+		} else {
+			return (int) Math.floor(value);
+		}
+	}
+	
+}
