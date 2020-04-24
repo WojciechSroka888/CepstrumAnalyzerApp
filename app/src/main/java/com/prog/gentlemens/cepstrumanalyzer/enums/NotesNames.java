2f48e8cd48d78298c091ad95eed97c7d76a99b38
Wojciech Sroka <ws.sroka@gmail.com>
2020-04-24 22:09:56 +0200

    changes in enum

--- app/src/main/java/com/prog/gentlemens/cepstrumanalyzer/enums/NotesNames.java
+++ app/src/main/java/com/prog/gentlemens/cepstrumanalyzer/enums/NotesNames.java
@@ -2,4 +2,5 @@ package com.prog.gentlemens.cepstrumanalyzer.enums;
 
 public enum NotesNames {
 	A, ais, H, C, cis, D, dis, E, F, fis, G, gis;
+	
 }
---
+++ app/src/main/java/com/prog/gentlemens/cepstrumanalyzer/enums/VowelName.java
@@ -0,0 +1,6 @@
+package com.prog.gentlemens.cepstrumanalyzer.enums;
+
+public enum VowelName {
+	NOT_SET, A, E, I, O, U, Y;
+	
+}
